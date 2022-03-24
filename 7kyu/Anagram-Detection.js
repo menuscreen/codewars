5fa54a4e8b231a01a248cfb4c541dc1eaa58b4f0
Michael Scott <70410703+menuscreen@users.noreply.github.com>
2022-03-24 12:51:35 -0400

    Create Anagram-Detection.js

---
+++ 7kyu/Anagram-Detection.js
@@ -0,0 +1,24 @@
+/*
+An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+Note: anagrams are case insensitive
+
+Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+
+Examples
+"foefet" is an anagram of "toffee"
+
+"Buckethead" is an anagram of "DeathCubeK"
+*/
+// write the function isAnagram
+var isAnagram = function(test, original) {
+  if(test.length !== original.length){
+    return false;
+  }else{
+    let sortedTest = test.toLowerCase().split('').sort().join('');
+    let sortedOriginal = original.toLowerCase().split('').sort().join('');
+    
+    return (sortedTest === sortedOriginal);
+  }
+};
+
