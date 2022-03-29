d365fe8e74c5b8a869bb1e5227c5aead0a16f795
menuscreen <70410703+menuscreen@users.noreply.github.com>
2022-03-29 12:34:45 -0400

    Solved Mumbling.js

---
+++ 7kyu/Mumbling.js
@@ -0,0 +1,39 @@
+/*
+    This time no story, no theory. The examples below show you how to write function accum:
+
+    Examples:
+        accum("abcd") -> "A-Bb-Ccc-Dddd"
+        accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+        accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+    The parameter of accum is a string which includes only letters from a..z and A..Z.
+*/
+function accum(s) {
+	tempArr = [];
+    tempArr = s.split('');
+    resultArr = [];
+    let count = 1;
+    for(let i = 0; i < tempArr.length; i++){ // iterate through each letter
+        for(let j = 0; j < count; j++){ // iterate through count (how many times to repeat)
+            if(j === 0){
+                resultArr.push(tempArr[i].toUpperCase());
+            }else{
+                resultArr.push(tempArr[i].toLowerCase());
+            }
+        }
+        if(i < tempArr.length - 1){
+            resultArr.push('-');
+        }
+        count++; // increment count for the next letter
+    }
+    newStr = resultArr.join('');
+    // console.log(newStr);
+    return newStr;
+}
+
+
+accum("ZpglnRxqenU") // == "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
+accum("NyffsGeyylB") // == "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"
+accum("MjtkuBovqrU") // == "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"
+accum("EvidjUnokmM") // == "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm"
+accum("HbideVbxncC") // == "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc"
