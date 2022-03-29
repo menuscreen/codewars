c139b8c9624211d30daddf506b2d85b509d09cff
menuscreen <70410703+menuscreen@users.noreply.github.com>
2022-03-29 12:43:34 -0400

    Added other examples

--- 7kyu/Mumbling.js
+++ 7kyu/Mumbling.js
@@ -37,3 +37,33 @@ accum("NyffsGeyylB") // == "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyy
 accum("MjtkuBovqrU") // == "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"
 accum("EvidjUnokmM") // == "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm"
 accum("HbideVbxncC") // == "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc"
+
+/*  Other Examples
+
+    function accum(s) {
+        return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
+    }
+
+
+    function accum(s) {
+        return s.split('').map((x,index) => x.toUpperCase()+Array(index+1).join(x.toLowerCase())).join('-');
+    }
+
+
+    function accum(str) {
+        var letters = str.split('');
+        var result = [];
+        for (var i = 0; i < letters.length; i++) {
+            result.push(letters[i].toUpperCase() + Array(i + 1).join(letters[i].toLowerCase()));
+        }
+        return result.join('-');
+    }
+
+
+    function accum(s) {
+        return [...s].map((element, index) => {
+            return element.toUpperCase() + element.toLowerCase().repeat(index);
+        }).join('-');
+    }
+
+*/
