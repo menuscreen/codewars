973117f880dd88264aee9fef96a8b1a68a6e8041
menuscreen <70410703+menuscreen@users.noreply.github.com>
2022-04-01 14:35:17 -0400

    Added other examples/notes

--- 7kyu/Shortest-Word.js
+++ 7kyu/Shortest-Word.js
@@ -17,3 +17,26 @@ function findShort(s){
 findShort("bitcoin take over the world maybe who knows perhaps") // == 3
 findShort("turns out random test cases are easier than writing out basic ones") // == 3
 findShort("Let's travel abroad shall we") // == 2
+
+/* other examples/solutions
+
+    function findShort(s){
+        return Math.min.apply(null, s.split(' ').map(w => w.length));
+    }
+
+
+    function findShort(s){
+        return Math.min(...s.split(" ").map (s => s.length));
+    }
+
+    function findShort(s){
+        var arr = s.split(' ');
+        var min = arr[0].length;
+        for (var i = 1; i < arr.length; i++) {
+            if (arr[i].length < min) {
+                min = arr[i].length;
+            }
+        }
+        return min
+    }
+*/
