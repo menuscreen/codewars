78bc6da4b4095a692299cd340e93ca01ae278bc4
menuscreen <70410703+menuscreen@users.noreply.github.com>
2022-04-02 12:19:28 -0400

    added more test cases and other example solutions

--- 7kyu/Find-the-stray-number.js
+++ 7kyu/Find-the-stray-number.js
@@ -32,3 +32,27 @@ function stray(numbers) {
 
 stray([1, 1, 2]) // == 2
 stray([17, 17, 10, 17]) // == 10
+stray([17, 17, 3, 17, 17, 17, 17]) // == 3
+stray([8, 1, 1, 1, 1, 1, 1]) // == 8)
+stray([-21, -21, -21, -21, -6, -21, -21]) // == -6
+stray([1, 1, 1, 1, 1, 1, 0]) // == 0
+stray([0, 0, 0, 7, 0, 0, 0]) // == 7
+
+/* other examples/solutions
+  
+    function stray(numbers) {
+        var a = numbers.sort();
+        
+        if(a[0] != a[1]) {
+            return a[0]
+        } 
+        return a[a.length-1]
+    }
+
+    function stray(nums){
+        let max = Math.max(...nums);
+        let min = Math.min(...nums);
+        return nums.filter(x => x == max).length == 1 ? max : min
+    }
+
+*/
