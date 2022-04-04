c6072b5b88d7d9ca95c0de49e538c5d0173acdc7
menuscreen <70410703+menuscreen@users.noreply.github.com>
2022-04-03 22:56:55 -0400

    added alternative solution note

--- 7kyu/Remove-the-minimum.js
+++ 7kyu/Remove-the-minimum.js
@@ -31,3 +31,9 @@ removeSmallest([1, 2, 3, 4, 5]) // == [2, 3, 4, 5]
 removeSmallest([5, 3, 2, 1, 4]) // == [5, 3, 2, 4]
 removeSmallest([2, 2, 1, 2, 1]) // == [2, 2, 2, 1]
 removeSmallest([]) // == []
+
+/* other examples/solutions
+
+    const removeSmallest = numbers => numbers.filter((n,i) => i !== numbers.indexOf(Math.min(...numbers)));
+
+*/
