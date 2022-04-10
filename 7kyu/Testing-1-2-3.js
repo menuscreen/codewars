760ff0a7d94fa4e6dc00356e85e7d147cc837b0d
menuscreen <70410703+menuscreen@users.noreply.github.com>
2022-04-10 17:51:47 -0400

    added other notes/solutions

--- 7kyu/Testing-1-2-3.js
+++ 7kyu/Testing-1-2-3.js
@@ -23,3 +23,37 @@ let number = function(array){
 
 number([]) // == []
 number(["a", "b", "c"]) // == ["1: a", "2: b", "3: c"]
+
+/* other examples/solutions
+
+
+    var number = function(array) {
+        return array.map(function (line, index) {
+            return (index + 1) + ": " + line;
+        });
+    }
+
+
+    let number = (a) => a.map((v, i) => `${i + 1}: ${v}`)
+
+
+    const number = array => array.map((n, i) => `${i + 1}: ${n}`);
+
+
+    function number(arr) {
+        return arr.map((e, i) => `${++i}: ${e}`);
+    }
+
+
+    var number = function(arr) {
+        var newArr = [];
+        if (arr.length === 0) {
+            return [];
+        } else {
+            for (var i = 0, len = arr.length; i < len; i += 1) {
+                newArr.push((i + 1) + ': ' + arr[i]);
+            }
+        }
+    return newArr;
+    }
+*/
