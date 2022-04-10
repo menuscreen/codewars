0f3c5abb059b14ac7b4b81b41cc57dca7c2f2e3b
menuscreen <70410703+menuscreen@users.noreply.github.com>
2022-04-10 17:11:20 -0400

    Solved Testing-1-2-3.js

--- 7kyu/Testing-1-2-3.js
+++ 7kyu/Testing-1-2-3.js
@@ -1,13 +1,25 @@
 /*
-Testing 1-2-3
-Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+    Testing 1-2-3
+    Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
 
-Write a function which takes a list of strings and returns each line prepended by the correct number.
+    Write a function which takes a list of strings and returns each line prepended by the correct number.
 
-The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+    The numbering starts at 1. The format is n: string. Notice the colon and space in between.
 
-Examples:
+    Examples:
 
-number([]) // => []
-number(["a", "b", "c"]) // => ["1: a", "2: b", "3: c"]
+        number([]) // => []
+        number(["a", "b", "c"]) // => ["1: a", "2: b", "3: c"]
 */
+let number = function(array){
+    if (array[0] === undefined){
+        return array;
+    }
+    for (let i = 0; i < array.length; i++){
+        array[i] = `${i+1}: ${array[i]}`;
+    }
+    return array;
+}
+
+number([]) // == []
+number(["a", "b", "c"]) // == ["1: a", "2: b", "3: c"]
