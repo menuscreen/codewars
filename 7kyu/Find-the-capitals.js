becb0a6501051e86945e903734bd27926925d5da
menuscreen <70410703+menuscreen@users.noreply.github.com>
2022-04-12 15:48:06 -0400

    Solved Find-the-capitals.js

--- 7kyu/Find-the-capitals.js
+++ 7kyu/Find-the-capitals.js
@@ -7,8 +7,31 @@
         Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
 */
 
-var capitals = function (word) {
-	// Write your code here
+const isUpperCase = (s) => /^[A-Z]/.test(s); // regex test returns true/false
+
+let capitals = function (word) {
+    let arrIndexOf = [];
+    for (let i = 0; i <= word.length; i++){
+        if (isUpperCase(word[i])){
+            arrIndexOf.push(i);
+        }
+    }
+    return arrIndexOf;
 };
 
 capitals('CodEWaRs') // == [0,3,4,6] 
+
+
+/* other examples  
+
+    var capitals = function (word) {
+        var caps = [];
+        for(var i = 0; i < word.length; i++) {
+            if(word[i].toUpperCase() == word[i]){
+                caps.push(i);
+            }
+        }
+        return caps;
+    };
+
+*/
