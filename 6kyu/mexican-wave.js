62eba7d40c9324b6eccfe0cd86ffcbecc63e2fdd
menuscreen <70410703+menuscreen@users.noreply.github.com>
2022-04-25 17:11:58 -0400

    Fixed indent/whitespace in notes

--- 6kyu/mexican-wave.js
+++ 6kyu/mexican-wave.js
@@ -70,8 +70,8 @@ function wave(str){
   for (let i = 0; i < str.length; i++) {
     let copy = str.split('');
     if(copy[i] !== ' ') {
-    copy[i] = copy[i].toUpperCase()
-    newArr.push(copy.join(''))
+        copy[i] = copy[i].toUpperCase()
+        newArr.push(copy.join(''))
     }
   }
   return newArr
