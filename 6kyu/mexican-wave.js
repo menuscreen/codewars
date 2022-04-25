bf9da26cdadadb34fbee98040b54c28de821acda
menuscreen <70410703+menuscreen@users.noreply.github.com>
2022-04-25 17:09:35 -0400

    Added notes

--- 6kyu/mexican-wave.js
+++ 6kyu/mexican-wave.js
@@ -48,3 +48,37 @@ wave("codewars") // == ["Codewars", "cOdewars", "coDewars", "codEwars", "codeWar
 wave("") // == []
 wave("two words") // == ["Two words", "tWo words", "twO words", "two Words", "two wOrds", "two woRds", "two worDs", "two wordS"]
 wave(" gap ") // == [" Gap ", " gAp ", " gaP "]
+
+/* Other examples/solutions
+
+
+function wave(str){
+    let result = [];
+    
+    str.split("").forEach((char, index) => {
+        if (/[a-z]/.test(char)) {
+            result.push(str.slice(0, index) + char.toUpperCase() + str.slice(index + 1));
+        }
+    });
+    
+    return result;
+}
+
+
+function wave(str){
+  let newArr = [];
+  for (let i = 0; i < str.length; i++) {
+    let copy = str.split('');
+    if(copy[i] !== ' ') {
+    copy[i] = copy[i].toUpperCase()
+    newArr.push(copy.join(''))
+    }
+  }
+  return newArr
+}
+
+
+var wave=w=>[...w].map((a,i)=>w.slice(0,i)+a.toUpperCase()+w.slice(i+1)).filter(a=>a!=w)
+
+
+*/
