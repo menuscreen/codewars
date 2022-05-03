49c6758fdb6cb8bb3172d7166128ceb2bd73e97b
menuscreen <70410703+menuscreen@users.noreply.github.com>
2022-05-03 12:34:19 -0400

    Solved write-number-in-expanded-form.js

--- 6kyu/write-number-in-expanded-form.js
+++ 6kyu/write-number-in-expanded-form.js
@@ -10,3 +10,24 @@
     NOTE: All numbers will be whole numbers greater than 0.
 
 */
+function expandedForm(num){
+    let strNumArr = num.toString().split('');
+    for (let i = 0; i < strNumArr.length; i++){
+        for (let j = strNumArr.length - i; j > 1; j--){
+            if (strNumArr[i] == '0'){
+                strNumArr.splice(i, 1);
+            }else{
+                strNumArr[i] += '0';
+            }
+        }
+    }
+    if (strNumArr[strNumArr.length - 1] === '0'){
+        strNumArr.pop();
+    }
+    let strNum = strNumArr.join(' + ');
+    return strNum;
+}
+
+expandedForm(12) // == '10 + 2'
+expandedForm(42) // == '40 + 2'
+expandedForm(70304) // == '70000 + 300 + 4'
