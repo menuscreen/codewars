d74a8184097ff4b00b29ddbb88c593d9531e8d03
menuscreen <70410703+menuscreen@users.noreply.github.com>
2022-05-06 16:26:41 -0400

    Added notes

--- 6kyu/detect-pangram.js
+++ 6kyu/detect-pangram.js
@@ -6,8 +6,9 @@
     Given a string, detect whether or not it is a pangram. Return True if it is, False if not.
     Ignore numbers and punctuation.
 */
-function isPangram(string){
-    let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+function isPangram(string){ // might consider refactor of least common letters of alphabet to be checked first
+    const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    // const leastCommonOrderAlphabet = ['z','x','q','j','k','g','b','v','p','y','w','f','n','c','y','k','d','r','h','s','n','i','o','a','t','e']
     for(let i = 0; i < alphabet.length; i++){
         if (string.toLowerCase().indexOf(alphabet[i]) !== -1){
             continue;
@@ -21,4 +22,27 @@ function isPangram(string){
 isPangram("The quick brown fox jumps over the lazy dog.") // == true
 isPangram("This is not a pangram.") // == false
 
-"This is not a pangram.".toLowerCase().indexOf('b')
+/* Other Examples/Solutions
+
+function isPangram(string){
+    string = string.toLowerCase();
+    return "abcdefghijklmnopqrstuvwxyz".split("").every(function(x){
+        return string.indexOf(x) !== -1;
+    });
+}
+
+
+function isPangram(string){
+    return 'abcdefghijklmnopqrstuvwxyz'
+        .split('')
+        .every((x) => string.toLowerCase().includes(x));
+}
+
+
+function isPangram(string) {
+    const alphabetList = [...'abcdefghijklmnopqrstuvwxyz'];
+    return alphabetList.every((letter) => string.toLowerCase().includes(letter));
+}
+
+
+*/
