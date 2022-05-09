2a068cbf47002ea318c875165ad398411ac7185e
menuscreen <70410703+menuscreen@users.noreply.github.com>
2022-05-09 12:48:45 -0400

    Solved who-likes-it.js

--- 6kyu/who-likes-it.js
+++ 6kyu/who-likes-it.js
@@ -17,7 +17,17 @@ Note: For 4 or more names, the number in "and 2 others" simply increases.
 */
 
 function likes(names) {
-    // TODO
+    if (names.length === 0){
+        return 'no one likes this';
+    }else if (names.length === 1){
+        return `${names[0]} likes this`;
+    }else if (names.length === 2){
+        return `${names[0]} and ${names[1]} like this`;
+    }else if (names.length === 3){
+        return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    }else{
+        return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+    }
 }
 
 likes([]) // =='no one likes this'
@@ -25,3 +35,4 @@ likes(['Peter']) // =='Peter likes this'
 likes(['Jacob', 'Alex']) // =='Jacob and Alex like this'
 likes(['Max', 'John', 'Mark']) // =='Max, John and Mark like this'
 likes(['Alex', 'Jacob', 'Mark', 'Max']) // =='Alex, Jacob and 2 others like this'
+
