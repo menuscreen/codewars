e671529a67de19f6f3b895972d11e0aa731de66e
menuscreen <70410703+menuscreen@users.noreply.github.com>
2022-05-11 17:07:56 -0400

    Solved count-characters-in-your-string.js

--- 6kyu/count-characters-in-your-string.js
+++ 6kyu/count-characters-in-your-string.js
@@ -5,3 +5,17 @@
     What if the string is empty? Then the result should be empty object literal, {}.
 
 */
+function count (string) {
+    const arr = string.split('');
+    let charCount = arr.reduce(function(obj, item){
+        if(!obj[item]){
+            obj[item] = 0;
+        }
+        obj[item]++;
+        return obj;
+    }, {});
+    return charCount;
+}
+
+count("aba") // == { a: 2, b: 1 }
+count("") // == {}
