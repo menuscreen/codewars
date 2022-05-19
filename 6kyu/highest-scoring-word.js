903263e651e4ff7a4b5a161145efcd996d26f2e4
menuscreen <70410703+menuscreen@users.noreply.github.com>
2022-05-19 11:45:53 -0400

    Solved highest-scoring-word.js

--- 6kyu/highest-scoring-word.js
+++ 6kyu/highest-scoring-word.js
@@ -10,15 +10,24 @@
     All letters will be lowercase and all inputs will be valid.
 */
 function high(x){
-    const alpha = ['abcdefghijklmnopqrstuvwxyz'];
-    let score = 0;
+    const alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    let wordScore;
+    let highScore = 0;
     let indexOfHighScore = 0;
+    x = x.split(' ');
     for (let i = 0; i < x.length; i++){
-        //split input by word
+        wordScore = 0;
         //check word score by letter
-        //if word score letter higher than previous replace indexscore with new index
+        for (let j = 0; j < x[i].length; j++){
+            wordScore += (alpha.indexOf(x[i][j]) + 1); // since index starts at 0 add 1 (a != 0)
+        }
+        //if word score higher than previous replace values highscore and index
+        if (wordScore > highScore){
+            highScore = wordScore;
+            indexOfHighScore = i;
+        }
     }
-    return //;
+    return x[indexOfHighScore];
 }
 
 high('man i need a taxi up to ubud') // == 'taxi'
