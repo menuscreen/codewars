064fffcdd8b5b964f24e23eda22b5bd82d285bde
menuscreen <70410703+menuscreen@users.noreply.github.com>
2022-05-31 14:47:30 -0400

    Added other example solutions to build-a-pile-of-cubes.js

--- 6kyu/build-a-pile-of-cubes.js
+++ 6kyu/build-a-pile-of-cubes.js
@@ -38,3 +38,35 @@ findNb(4183059834009) // == 2022
 findNb(24723578342962) // == -1
 findNb(135440716410000) // == 4824
 findNb(40539911473216) // == 3568
+
+/* Other solutions  
+
+    function findNb(m) {
+        var n = 0
+        while (m > 0) m -= ++n**3
+        return m ? -1 : n
+    }
+
+
+    function findNb(m) {
+        let n = 0;
+        let sum = 0;
+        while (sum < m) {
+            n++;
+            sum += Math.pow(n, 3);
+        }
+        return sum === m ? n : -1;
+    }
+
+
+    // this is based on the formula that the sum of the first n cubes equals (n * (n + 1) / 2) ^ 2
+    // also, the sum of the first n cubes is always a square
+    function findNb(m) {
+        m = Math.sqrt(m) * 2;
+        if (m != parseInt(m)){
+            return -1;
+        }
+        var result = parseInt(Math.sqrt(m));
+        return (result * (result + 1) == m) ? result : -1;
+    }
+*/
