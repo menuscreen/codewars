4805c66cd3de9dead7bfa2bb8acd18975a856bfb
menuscreen <70410703+menuscreen@users.noreply.github.com>
2022-06-03 12:21:29 -0400

    Added other example solutions to your-order-please.js

--- 6kyu/your-order-please.js
+++ 6kyu/your-order-please.js
@@ -37,3 +37,38 @@ function order(words){
 order("is2 Thi1s T4est 3a") // == "Thi1s is2 3a T4est"
 order("4of Fo1r pe6ople g3ood th5e the2") // == "Fo1r the2 g3ood 4of th5e pe6ople"
 order("") // == "" empty input should return empty string
+
+/* other solutions
+
+    function order(words){
+        return words.split(' ').sort(function(a, b){
+            return a.match(/\d/) - b.match(/\d/);
+        }).join(' ');
+    }    
+
+
+    var reg = /\d/;
+    function order(words){
+        return words.split(' ').sort(comparator).join(' ');
+    }
+    function comparator(word, nextWord) {
+        return +word.match(reg) - +nextWord.match(reg)
+    }
+
+
+    function order(words){
+        var array = words.split(' ');
+        var sortedArray = [];
+        for(i = 0; i <= array.length; i++) {
+            for(j = 0; j < array.length; j++) {
+                if(array[j].indexOf(i) >= 0) {
+                    sortedArray.push(array[j]);
+                }
+            }
+        }
+        return sortedArray.join(' ');
+    }
+
+
+    
+*/
