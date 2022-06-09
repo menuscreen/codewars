ee3b43c72aff40ad8b192de7a4564d3513c3bfc8
menuscreen <70410703+menuscreen@users.noreply.github.com>
2022-06-09 13:24:02 -0400

    Added test cases

--- 6kyu/sum-raised-eureka.js
+++ 6kyu/sum-raised-eureka.js
@@ -27,3 +27,12 @@
 function sumDigPow(a, b) {
     // Your code here
 }
+
+
+sumDigPow(1, 10) // ==  [1, 2, 3, 4, 5, 6, 7, 8, 9]
+sumDigPow(1, 100) // ==  [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]
+sumDigPow(10, 100) // ==   [89]
+sumDigPow(90, 100) // ==  []
+sumDigPow(90, 150) // ==  [135]
+sumDigPow(50, 150) // ==  [89, 135]
+sumDigPow(10, 150) // ==  [89, 135]
