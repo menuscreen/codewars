0e57f589f698a018aef123d1179503ed07389404
menuscreen <70410703+menuscreen@users.noreply.github.com>
2022-06-16 19:36:49 -0400

    Added test cases

--- 6kyu/consecutive-strings.js
+++ 6kyu/consecutive-strings.js
@@ -28,3 +28,14 @@
 function longestConsec(strarr, k) {
     // your code
 }
+
+
+longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2) // ==  "abigailtheta"
+longestConsec(["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"], 1) // == "oocccffuucccjjjkkkjyyyeehh"
+longestConsec([], 3) // == ""
+longestConsec(["itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2) // == "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck")
+longestConsec(["wlwsasphmxx","owiaxujylentrklctozmymu","wpgozvxxiu"], 2) // == "wlwsasphmxxowiaxujylentrklctozmymu"
+longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], -2) // == "")
+longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 3) // ==  "ixoyx3452zzzzzzzzzzzz"
+longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 15) // ==  ""
+longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 0) // ==  ""
