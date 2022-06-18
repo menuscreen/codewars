cda65046f4fc65357e4154e8e384ad60e919dc55
menuscreen <70410703+menuscreen@users.noreply.github.com>
2022-06-18 00:30:28 -0400

    added other examples

--- 6kyu/consecutive-strings.js
+++ 6kyu/consecutive-strings.js
@@ -57,3 +57,49 @@ longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], -2) // == "")
 longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 3) // ==  "ixoyx3452zzzzzzzzzzzz"
 longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 15) // ==  ""
 longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 0) // ==  ""
+
+/* other examples
+
+function longestConsec(strarr, k) {
+    var longest = "";
+    for(var i=0;k>0 && i<=strarr.length-k;i++){
+      var tempArray = strarr.slice(i,i+k);
+      var tempStr = tempArray.join("");
+      if(tempStr.length > longest.length){
+        longest = tempStr;
+      }
+    }
+    return longest;
+}
+
+
+function longestConsec(strarr, k) {
+  if (k <= 0 || k > strarr.length) {
+    return '';
+  }
+  
+  return strarr.reduce((long, item, i) => {
+    const currString = strarr.slice(i, i + k).join('');
+    return (currString.length > long.length)
+      ? currString
+      : long;
+  }, '');
+}
+
+
+function longestConsec(strarr, k) {
+    if (k <= 0 || k > strarr.length) {
+      return "";
+    }
+    
+    return strarr
+    .map((value, index) => (
+      strarr.slice(index, index+k).join('')
+    ))
+    .reduce((longest, current) => current.length > longest.length ? current : longest)
+    
+}
+
+
+
+*/
