bddb0bab00c116f6c5bf23d91e8e1c6e681ae7bf
menuscreen <70410703+menuscreen@users.noreply.github.com>
2022-06-21 14:39:58 -0400

    Solved simple-encryption1.js

--- 6kyu/simple-encryption1.js
+++ 6kyu/simple-encryption1.js
@@ -20,9 +20,71 @@
 */
 
 function encrypt(text, n) {
-
+    if (text === '' || text === null || n <= 0) {
+        return text;
+    }
+    for (let i = 0; i < n; i++) {
+        let textArr = text.split('');
+        let oddArr = [];
+        let evenArr = [];
+        for (let j = 0; j < textArr.length; j++) {
+            if (j % 2 === 0) {
+                evenArr.push(textArr[j]);
+            } else {
+                oddArr.push(textArr[j]);
+            }
+        }
+        text = oddArr.join('') + evenArr.join('')
+    }
+    return text;
 }
 
 function decrypt(encryptedText, n) {
-
+    if (encryptedText === '' || encryptedText === null || n <= 0) {
+        return encryptedText;
+    }
+    for (let i = 0; i < n; i++) {
+        let mid = encryptedText.length / 2;
+        if (encryptedText.length % 2 === 0) {
+            mid = Math.ceil(mid);
+        }else {
+            mid = Math.floor(mid);
+        }
+        let oddStr = encryptedText.substring(0, mid);
+        let evenStr = encryptedText.substring(mid);
+        let oddArr = oddStr.split('');
+        let evenArr = evenStr.split('');
+        let textArr = [];
+        for (let j = 0; j < encryptedText.length; j++) {
+            if (j % 2 === 0) {
+                textArr.push(evenArr.shift());
+            } else {
+                textArr.push(oddArr.shift());
+            }
+        }
+        encryptedText = textArr.join('');
+    }
+    return encryptedText;
 }
+
+
+encrypt("This is a test!", 0) // == "This is a test!"
+encrypt("This is a test!", 1) // == "hsi  etTi sats!"
+encrypt("This is a test!", 2) // == "s eT ashi tist!"
+encrypt("This is a test!", 3) // == " Tah itse sits!"
+encrypt("This is a test!", 4) // == "This is a test!"
+encrypt("This is a test!", -1) // == "This is a test!"
+encrypt("This kata is very interesting!", 1) // == "hskt svr neetn!Ti aai eyitrsig"
+ 
+decrypt("This is a test!", 0) // == "This is a test!"
+decrypt("hsi  etTi sats!", 1) // == "This is a test!"
+decrypt("s eT ashi tist!", 2) // == "This is a test!"
+decrypt(" Tah itse sits!", 3) // == "This is a test!"
+decrypt("This is a test!", 4) // == "This is a test!"
+decrypt("This is a test!", -1) // == "This is a test!"
+decrypt("hskt svr neetn!Ti aai eyitrsig", 1) // == "This kata is very interesting!"
+
+encrypt("", 0) // == ""
+decrypt("", 0) // == ""
+encrypt(null, 0) // == null
+decrypt(null, 0) // == null
