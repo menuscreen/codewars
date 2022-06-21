4d4827b788919ddf8f0f4fbf7a0ba02960c28d8d
menuscreen <70410703+menuscreen@users.noreply.github.com>
2022-06-21 14:54:13 -0400

    Added other example solutions

--- 6kyu/simple-encryption1.js
+++ 6kyu/simple-encryption1.js
@@ -88,3 +88,112 @@ encrypt("", 0) // == ""
 decrypt("", 0) // == ""
 encrypt(null, 0) // == null
 decrypt(null, 0) // == null
+
+/* Other example solutions
+
+function encrypt(text, n) {
+  console.log(text, n);
+  if (!text || n <= 0) return text; 
+  while (n--) {
+    let ans = '';
+    for (let i = 1; i < text.length; i += 2) {
+      ans += text[i];
+    }
+    for (let i = 0; i < text.length; i += 2) {
+      ans += text[i];
+    }
+    text = ans;
+  }
+  return text;
+}
+
+function decrypt(encryptedText, n) {
+  if (!encryptedText || n <= 0) return encryptedText;
+  const ans = new Array(encryptedText.length);
+  while (n--) {
+    let j = 0;
+    for (let i = 1; i < ans.length; i += 2) {
+      ans[i] = encryptedText[j++];
+    }
+    for (let i = 0; i < ans.length; i += 2) {
+      ans[i] = encryptedText[j++];
+    }
+    encryptedText = ans.join('');
+  }
+  return encryptedText;
+}
+
+
+
+function encrypt(text, n) {
+  for (let i = 0; i < n; i++) {
+    text = text && text.replace(/.(.|$)/g, '$1') + text.replace(/(.)./g, '$1') 
+  }
+  return text
+}
+
+function decrypt(text, n) {
+  let l = text && text.length / 2 | 0
+  for (let i = 0; i < n; i++) {
+    text = text.slice(l).replace(/./g, (_, i) => _ + (i < l ? text[i] : ''))
+  }
+  return text
+}
+
+
+
+function encrypt(text, n) {
+  if (!text || !text.length || n <= 0)
+  {
+    return text;
+  }
+
+  var res = "";
+  var oth = "";
+
+  for (var i = 0; i < text.length; ++i)
+  {
+    if (i % 2 == 0)
+    {
+      oth += text[i];
+    }
+    else
+    {
+      res += text[i];
+    }
+  }
+
+  return encrypt(res + oth, --n);
+}
+
+function decrypt(encryptedText, n) {
+  if (!encryptedText || !encryptedText.length || n <= 0)
+  {
+    return encryptedText;
+  }
+
+  var first = encryptedText.slice(0, encryptedText.length / 2);
+  var second = encryptedText.slice(encryptedText.length / 2);
+  
+  var res = "";
+  var i = 0;
+  var j = 0;
+
+  while (res.length < encryptedText.length)
+  {
+    if (res.length % 2 == 0)
+    {
+      res += second[i];
+      i++;
+    }
+    else
+    {
+      res += first[j];
+      j++;
+    }
+  }
+
+  return decrypt(res, --n);
+}
+
+*/
