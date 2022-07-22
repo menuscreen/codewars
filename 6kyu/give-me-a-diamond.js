826ff763ae37f3bfcfe2771d7d3890080300dd95
menuscreen <70410703+menuscreen@users.noreply.github.com>
2022-07-22 15:49:20 -0400

    added notes

--- 6kyu/give-me-a-diamond.js
+++ 6kyu/give-me-a-diamond.js
@@ -82,12 +82,58 @@ function diamond(n){
     const diam = top + center + bottom;
 
     return diam;
-}
+};
 
 console.log(diamond(11));
-console.log(diamond(1)) // -> "*\n"
-console.log(diamond(3)) // -> " *\n***\n *\n"
-console.log(diamond(5)) // -> "  *\n ***\n*****\n ***\n  *\n"
-console.log(diamond(2)) // -> null
-console.log(diamond(-3)) // -> null
-console.log(diamond(0)) // -> null
+console.log(diamond(1)); // -> "*\n"
+console.log(diamond(3)); // -> " *\n***\n *\n"
+console.log(diamond(5)); // -> "  *\n ***\n*****\n ***\n  *\n"
+console.log(diamond(2)); // -> null
+console.log(diamond(-3)); // -> null
+console.log(diamond(0)); // -> null
+
+/* other examples
+
+function diamond (n) {
+  if (n <= 0 || n % 2 === 0) return null
+  str = ''
+  for (let i = 0; i < n; i++) { 
+    let len = Math.abs((n-2*i-1)/2)
+    str += ' '.repeat(len)
+    str += '*'.repeat(n-2*len)
+    str += '\n'
+  }
+  return str
+}
+
+
+
+function diamond(n){
+  if( n%2==0 || n<1 ) return null
+  var x=0, add, diam = line(x,n);
+  while( (x+=2) < n ){
+    add = line(x/2,n-x);
+    diam = add+diam+add;
+  }
+  return diam;
+}//z.
+
+function repeat(str,x){return Array(x+1).join(str); }
+function line(spaces,stars){ return repeat(" ",spaces)+repeat("*",stars)+"\n"; }
+
+
+
+function diamond(n){
+  if (n < 0 || !(n % 2)) return null; 
+  const middleIndex = Math.floor(n / 2);
+  
+  return Array.apply(null, {length: n})
+      .map((el, index) => {
+        const indentation = Math.abs(index - middleIndex);
+        const numberOfAsterisks = n - indentation * 2;
+        return Array(indentation + 1).join(' ') + Array(numberOfAsterisks + 1).join('*');
+      })
+      .join('\n') + '\n';
+}
+
+*/
