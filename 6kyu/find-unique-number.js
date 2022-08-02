a12f475698219428c028693b11e3625e96a1a46c
menuscreen <70410703+menuscreen@users.noreply.github.com>
2022-08-02 12:37:01 -0400

    Added skeleton and test cases

--- 6kyu/find-unique-number.js
+++ 6kyu/find-unique-number.js
@@ -3,8 +3,18 @@
 
     findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
     findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
-    
+
     It’s guaranteed that array contains at least 3 numbers.
 
     The tests contain some very huge arrays, so think about performance.
 */
+function findUniq(arr) {
+  // do magic
+}
+
+findUniq([ 1, 0, 0 ]) // == 1
+findUniq([ 0, 1, 0 ]) // == 1
+findUniq([ 0, 0, 1 ]) // == 1
+findUniq([ 1, 1, 1, 2, 1, 1 ]) // == 2
+findUniq([ 1, 1, 2, 1, 1 ]) // == 2
+findUniq([ 3, 10, 3, 3, 3 ]) // == 10
