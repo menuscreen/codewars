6cd93d3ea02275e241cf9b4b1b348c3ee728cbf7
menuscreen <70410703+menuscreen@users.noreply.github.com>
2022-08-04 18:53:21 -0400

    Passing some tests, failing others

--- 6kyu/find-unique-number.js
+++ 6kyu/find-unique-number.js
@@ -9,8 +9,30 @@
     The tests contain some very huge arrays, so think about performance.
 */
 function findUniq(arr) {
-  // do magic
-}
+    const arrLength = arr.length;
+
+    let i = 0;
+    let j = 1;
+    let k = 2;
+
+    let left = arr[i];
+    let middle = arr[j];
+    let right = arr[k];
+
+    while (right < arrLength){
+        if (left == middle && middle == right){ // [ 0, 0, 0 ]
+            left = arr[i++];
+            middle = arr[j++];
+            right = arr[k++];
+        } else if (left != middle && middle == right){ // [ 1, 0, 0 ]
+            return left;
+        } else if (middle != left && left == right){ // [ 0, 1, 0 ]
+            return middle;
+        } else { // [ 0, 0, 1 ]
+            return right;
+        }
+    };
+};
 
 findUniq([ 1, 0, 0 ]) // == 1
 findUniq([ 0, 1, 0 ]) // == 1
