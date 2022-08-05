cff0e4e8414ab162a01ea6fcc38655f7423b4511
menuscreen <70410703+menuscreen@users.noreply.github.com>
2022-08-04 21:05:18 -0400

    Other solution notes

--- 6kyu/find-unique-number.js
+++ 6kyu/find-unique-number.js
@@ -41,3 +41,21 @@ findUniq([ 1, 1, 1, 2, 1, 1 ]) // == 2
 findUniq([ 1, 1, 2, 1, 1 ]) // == 2
 findUniq([ 3, 10, 3, 3, 3 ]) // == 10
 
+/*
+function findUniq(arr) {
+  arr.sort((a,b)=>a-b);
+  return arr[0]==arr[1]?arr.pop():arr[0]
+}
+
+
+function findUniq(arr) {
+  return arr.find(n => arr.indexOf(n) === arr.lastIndexOf(n));
+}
+
+
+function findUniq(arr) {
+  let [a,b,c] = arr.slice(0,3);
+  if( a != b && a!=c ) return a;
+  for( let x of arr ) if( x!=a ) return x
+}
+*/
